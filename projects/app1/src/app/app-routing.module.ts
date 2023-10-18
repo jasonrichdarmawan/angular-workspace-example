bf2181e5d9efa55a42e61430bb61e114d664fa77
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-10-18 09:48:45 +0700

    chore: proof of concept routing

--- projects/app1/src/app/app-routing.module.ts
+++ projects/app1/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { View1Component } from './view1/view1.component';
-import { View2Component } from './view2/view2.component';
 
 const routes: Routes = [
-  { path: 'app1/one', component: View1Component },
-  { path: 'app1/two', component: View2Component },
+  { path: 'app1/one', loadChildren: () => import("./view1/view1.module").then(m => m.View1Module) },
+  { path: 'app1/two', loadChildren: () => import("./view2/view2.module").then(m => m.View2Module) },
   { path: 'app1', redirectTo: 'app1/one' }
 ];
 
---
+++ projects/app1/src/app/view1/view1.module.ts
@@ -0,0 +1,29 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { View1RoutingModule } from './view1-routing.module';
+import { View1Component } from './view1.component';
+import { NavSharedModule } from '../nav/nav.module';
+
+
+@NgModule({
+  declarations: [
+    View1Component,
+  ],
+  imports: [
+    CommonModule,
+    View1RoutingModule,
+    NavSharedModule.forRoot(),
+  ],
+})
+export class View1Module { }
+
+@NgModule({})
+export class View1SharedModule {
+  static forRoot(): ModuleWithProviders<View1Module> {
+    return {
+      ngModule: View1Module,
+      providers: [],
+    }
+  }
+}
